71abc4e8cd99dde7964b608ac4da18c07d8dcf5a
Leo Guedes <leoguedesdev@gmail.com>
2024-03-15 20:22:46 -0300

    Adicionando a classe CurrentAccount

---
+++ src/main/java/LeoGuedex/com/github/ProjetoBancario/domain/CurrentAccount.java
@@ -0,0 +1,28 @@
+package LeoGuedex.com.github.ProjetoBancario.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrentAccount extends Account {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private List<Card> cards;
+  private List<Checkbook> checkbooks;
+  private List<Loan> loans;
+
+}
