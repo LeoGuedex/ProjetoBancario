dec943c3d6bf5ed19730b67cebb52121a2892748
Leo Guedes <leoguedesdev@gmail.com>
2024-03-15 20:24:05 -0300

    Adicionando a classe Deposit

---
+++ src/main/java/LeoGuedex/com/github/ProjetoBancario/domain/Deposit.java
@@ -0,0 +1,29 @@
+package LeoGuedex.com.github.ProjetoBancario.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Deposit {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private BigDecimal totalDeposited;
+  private LocalDateTime whenCreated;
+  private Account conta;
+
+}
